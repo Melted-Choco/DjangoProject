4c68b1fd04630a8934a36b34d76436131eeb7c9b
정태문 <73479963+Melted-Choco@users.noreply.github.com>
2025-05-17 23:15:54 +0900

    Update Files
    Add admin page & Add data to Model(database)
    Create some View(detail, results, vote) with only a form

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -6,3 +6,16 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, World. You're at the polls index.")
+
+# Show question_text
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+# Show the result of Question
+def results(request, question_id):
+    response = "You're looking at the results of quesiton %s."
+    return HttpResponse(response % question_id)
+
+# Function of Voting
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
