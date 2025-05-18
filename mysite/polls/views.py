efdf2aee76d2379dd8644b79eaa250c7df4d31b4
정태문 <73479963+Melted-Choco@users.noreply.github.com>
2025-05-18 22:47:23 +0900

    Update Files
    Update 'vote' view & 'results' view
    Add 'results' template

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 def index(request):
     # Receive data in the latest order based on pub_date
@@ -29,9 +30,21 @@ def detail(request, question_id):
 
 # Show the result of Question
 def results(request, question_id):
-    response = "You're looking at the results of quesiton %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 # Function of Voting
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice']) # Get key & value
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form
+        return render(request, 'polls/detail.html', {
+            'question' : question,
+            'error_message' : "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
